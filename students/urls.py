383ab9e2d149e296f09210713d3bba1b286b0169
Nikolay Bayborodin <nbbrdn@gmail.com>
2023-02-11 10:45:13 +1000

    Init commit

---
+++ students/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from api import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    re_path(r"^api/students/$", views.students_list),
+    re_path(r"^api/students/(\d+)$", views.students_detail),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
